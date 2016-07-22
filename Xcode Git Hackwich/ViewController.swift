aad31351daca9c09feb462acfbcc6219d2455cf8
Tom Bredemeier <tbredemeier@barrington220.org>
2016-07-22 08:18:01 -0500

    Initial Commit

---
+++ Xcode Git Hackwich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode Git Hackwich
+//
+//  Created by tbredemeier on 7/22/16.
+//  Copyright © 2016 Zephyr Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
